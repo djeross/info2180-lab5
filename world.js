4c952ce428b4c2ddf8f5524b8cceeb8486d64454
Rodje Ross <rodjeross10@yahoo.com>
2020-11-16 22:29:40 -0500

    Completed lab files

--- world.js
+++ world.js
@@ -2,7 +2,7 @@ window.addEventListener('click',()=>{
 
     document.querySelector('#lookup').addEventListener('click',(e)=>{
         e.preventDefault();
-        let country_req=document.querySelector("#country").value;
+        let country_req=document.querySelector("#country").value.replace( /(<([^>]+)>)/ig, '');
         let url="http://localhost/info2180-lab5/world.php"+"?country="+country_req;
 
         fetch(url)
@@ -15,24 +15,33 @@ window.addEventListener('click',()=>{
             }
         })
         .then((data)=>{
-            document.querySelector('#result').innerHTML=data;
-            
+            document.querySelector('#result').innerHTML=data; 
         })
         .catch(error=>{
             alert("error in retrieving data");
-        })
+        });
     });
 
+    document.querySelector('#cities').addEventListener('click',(e)=>{
+        e.preventDefault();
+        let cities_req=document.querySelector("#country").value.replace( /(<([^>]+)>)/ig, '');
+        let url="http://localhost/info2180-lab5/world.php"+"?country="+cities_req+"&context=cities";
 
-
-
-
-
-
-
-
-
-
-
+        fetch(url)
+        .then((response)=>{
+            if(response.ok){
+                return response.text();
+            }
+            else{
+                Promise.reject("Error in retrieving data");
+            }
+        })
+        .then((data)=>{
+            document.querySelector('#result').innerHTML=data;  
+        })
+        .catch(error=>{
+            alert("error in retrieving data");
+        });
+    });
 
 });
--- world.php
+++ world.php
@@ -4,50 +4,86 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
+/*Establishing database connection*/
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
+
+$context= filter_input(INPUT_GET,"context",FILTER_SANITIZE_STRING);
+
 /* Query for getting countries*/
-$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :condition");
-$stmt->bindParam(":condition",$condition,PDO::PARAM_STR);
-$myquery = filter_input(INPUT_GET,"country",FILTER_SANITIZE_STRING);
-$condition = "%$myquery%";
-$stmt->execute();
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (!isset($context)){
+  /* Query for getting countries*/
+  $statement = $conn->prepare("SELECT * FROM countries WHERE name LIKE :condition");
+  $statement->bindParam(":condition",$condition,PDO::PARAM_STR);
+  $myquery = filter_input(INPUT_GET,"country",FILTER_SANITIZE_STRING);
+  $condition = "%$myquery%";
+  $statement->execute();
+  $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+else{
+  /*Query for getting citites*/
+  $statement1 = $conn->prepare("SELECT cities.name, cities.district, cities.population 
+                            FROM cities join countries on cities.country_code=countries.code 
+                            WHERE countries.name= :myquery1");
+  $statement1->bindParam(":myquery1",$myquery1,PDO::PARAM_STR);
+  $myquery1 = filter_input(INPUT_GET,"country",FILTER_SANITIZE_STRING);
+  $statement1->execute();
+  $results1 = $statement1->fetchAll(PDO::FETCH_ASSOC);
+}
 
-
-/* Query for countries table heading*/
-/*$table_headdings = $conn->query("SELECT * FROM `INFORMATION_SCHEMA`.`COLUMNS` 
-WHERE `TABLE_SCHEMA`='world' AND `TABLE_NAME`='countries' ");
-
-$headings = $table_headdings->fetchAll(PDO::FETCH_ASSOC);
-echo (var_dump($headings));*/
 ?>
 
-<style>
-  #table1 th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color:#a79344 ;color: white;}
-  #table1{border-collapse: collapse;width: 100%;}
-  #table1 td, #table1 th {border: 1px solid rgb(202, 201, 201);padding: 10px;}
-  #table1 tr:nth-child(even){background-color: #d6d4d4;}
-</style>
-
-<table id="table1">
-    <caption><h2>Table showing a list of Countries, the continent on which they are located, their date of independence and head of state</h2></caption>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Continent</th>
-        <th>Independence</th>
-        <th>Head Of State</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($results as $country): ?>
+<?php if(isset($context)):?>
+  <table class="displaytable">
+      <caption><h2>Table showing a list of Cities and corresponding information for a Country entered </h2></caption>
+      <thead>
         <tr>
-          <td><?php echo $country['name']; ?></td>
-          <td><?php echo $country['continent']; ?></td>
-          <td><?php echo $country['independence_year']; ?></td>
-          <td><?php echo $country['head_of_state'];?>
+          <th class="th1">Name</th>
+          <th class="th2">District</th>
+          <th class="th3">Population</th>
         </tr>
-      <?php endforeach; ?>
-  </tbody>
-</table>
+      </thead>
+      <tbody>
+        <?php foreach ($results1 as $city): ?>
+          <tr>
+            <td><?php echo $city['name']; ?></td>
+            <td><?php echo $city['district']; ?></td>
+            <td><?php echo $city['population']; ?></td>
+          </tr>
+        <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif;?>
+<?php if(!isset($context)):?>
+  <table class="displaytable">
+      <caption><h2>Table showing a list of Countries, the continent on which they are located, their date of independence and head of state</h2></caption>
+      <thead>
+        <tr>
+          <th class="th1">Name</th>
+          <th class="th2">Continent</th>
+          <th class="th3">Independence</th>
+          <th class="th4">Head Of State</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($results as $country): ?>
+          <tr>
+            <td><?php echo $country['name']; ?></td>
+            <td><?php echo $country['continent']; ?></td>
+            <td><?php echo $country['independence_year']; ?></td>
+            <td><?php echo $country['head_of_state'];?>
+          </tr>
+        <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif;?>
+
+
+
+
+
+<!--Query for countries table heading*/
+/*$table_headdings = $conn->query("SELECT * FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+WHERE `TABLE_SCHEMA`='world' AND `TABLE_NAME`='countries' ");
+$headings = $table_headdings->fetchAll(PDO::FETCH_ASSOC);
+echo (var_dump($headings));*/-->
